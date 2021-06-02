3591270fd4e497c6cf29a85ae640bab23cd9ec73
Raman Sharma <rs9463194297@gmail.com>
2021-06-02 12:05:16 +0530

    user can delete column

--- src/components/CreateTable/index.js
+++ src/components/CreateTable/index.js
@@ -27,16 +27,31 @@ const CreateTable = ({ colNo }) => {
       prevRowsData.map((row) => row.push("") && row)
     );
   };
+  const deleteColumn = (index) => {
+    if (columns.length === 1) {
+      return toast.dark("There Should be atleast 1 column!");
+    }
+    setColumns((prevColumns) =>
+      prevColumns.filter((col) => prevColumns.indexOf(col) !== index)
+    );
+
+    setRowsData((prevRowsData) =>
+      prevRowsData.map((row) => {
+        const filteredRows = row.filter((rw, i) => i !== index);
+        return filteredRows;
+      })
+    );
+  };
 
   useEffect(() => {
-    if (columns.length < colNo) {
-      let array = [""];
-      for (let i = 1; i < colNo; i++) {
-        setColumns([...columns, `Column ${i + 1}`]);
-        array.push("");
-      }
-      setRowsData([array]);
+    let array = [""];
+    for (let i = 1; i < colNo; i++) {
+      setColumns([...columns, `Column ${i + 1}`]);
+      array.push("");
     }
+    setRowsData([array]);
+
+    console.log("working");
     setGenerating(false);
   }, []);
   return (
@@ -64,55 +79,79 @@ const CreateTable = ({ colNo }) => {
       <Row>
         <Col md={12} className="px-5">
           {!generating ? (
-            <Table responsive className="mt-5 h-100">
-              <thead className="bg-dark text-white">
-                <tr>
-                  <th className="d-flex align-items-center justify-content-center py-3 pb-2 border-0">
-                    Sno.
-                  </th>
-                  {columns.map((col, index) => (
-                    <th key={index}>
-                      <input
-                        type="text"
-                        className="form-control border-0 text-center bg-dark text-white"
-                        style={{ outline: "none", boxShadow: "none" }}
-                        readOnly={readOnly}
-                        onFocus={() => setReadOnly(false)}
-                        onBlur={() => setReadOnly(true)}
-                        value={columns[index]}
-                        onChange={(e) =>
-                          setColumns(
-                            columns.map((coln, id) =>
-                              id === index ? e.target.value : coln
-                            )
-                          )
-                        }
-                      />
-                    </th>
-                  ))}
-                </tr>
-              </thead>
-              <tbody>
-                {rowsData.map((data, index) => (
-                  <tr key={index + 5}>
-                    <td className="text-center">{index + 1}</td>
-                    {data.map((row, index2) => (
-                      <td key={index2 + 988}>
-                        <input
-                          type="text"
-                          className="form-control text-center"
-                          placeholder={`Enter field`}
-                          value={rowsData[index][index2]}
-                          onChange={(e) =>
-                            handleChange(e.target.value, index, index2)
-                          }
-                        />
+            <>
+              <Table responsive className="mt-5">
+                <tbody>
+                  <tr>
+                    <td>#</td>
+                    {columns.map((col, index) => (
+                      <td key={index + 999999}>
+                        <Button
+                          type="button"
+                          className="btn-block w-100"
+                          onClick={() => deleteColumn(index)}
+                          variant="outline-danger"
+                        >
+                          Delete
+                        </Button>
                       </td>
                     ))}
                   </tr>
-                ))}
-              </tbody>
-            </Table>
+                </tbody>
+              </Table>
+              <Table responsive className="mt-2 h-100">
+                <thead className="bg-dark text-white">
+                  <tr>
+                    <th
+                      scope="col"
+                      className="d-flex align-items-center justify-content-center py-3 pb-2 border-0"
+                    >
+                      Sno.
+                    </th>
+                    {columns.map((col, index) => (
+                      <th key={index} scope="col">
+                        <input
+                          type="text"
+                          className="form-control border-0 text-center bg-dark text-white"
+                          style={{ outline: "none", boxShadow: "none" }}
+                          readOnly={readOnly}
+                          onFocus={() => setReadOnly(false)}
+                          onBlur={() => setReadOnly(true)}
+                          value={columns[index]}
+                          onChange={(e) =>
+                            setColumns(
+                              columns.map((coln, id) =>
+                                id === index ? e.target.value : coln
+                              )
+                            )
+                          }
+                        />
+                      </th>
+                    ))}
+                  </tr>
+                </thead>
+                <tbody>
+                  {rowsData.map((data, index) => (
+                    <tr key={index + 5}>
+                      <td className="text-center">{index + 1}</td>
+                      {data.map((row, index2) => (
+                        <td key={index2 + 988}>
+                          <input
+                            type="text"
+                            className="form-control text-center"
+                            placeholder={`Enter field`}
+                            value={rowsData[index][index2]}
+                            onChange={(e) =>
+                              handleChange(e.target.value, index, index2)
+                            }
+                          />
+                        </td>
+                      ))}
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            </>
           ) : (
             <h1 className="text-center my-5">Generating...</h1>
           )}
